6abc7083807935f3f77894fd356b9b2023310ece
Eunsoo Lee <eunsoogi.lee@gmail.com>
2021-10-08 11:36:48 +0900

    Change launcher name

--- src/scripts/launcher.py
+++ src/scripts/launcher.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from rosnode import KMNode
 
-if __name__ == "__main__":
-    km_node = KMNode(node_name='km_node')
+
+if __name__ == '__main__':
+    km_node = KMNode(node_name='KM_node')
     km_node.spin()
